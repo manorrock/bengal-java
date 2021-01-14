85507797f0309a1d1241b48e88a1aa10fa209d26
Manfred Riem <mriem@manorrock.com>
2021-01-14 07:46:59 -0700

    Fixes issue #2 - Add MessageObjectEmitter

---
+++ message/core/src/main/java/com/manorrock/bengal/message/core/MessageObject.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ */
+package com.manorrock.bengal.message.core;
+
+/**
+ * A Bengal Message - 'object'
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class MessageObject {
+}
---
+++ message/emitter/src/main/java/com/manorrock/bengal/message/emitter/MessageObjectEmitter.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ */
+package com.manorrock.bengal.message.emitter;
+
+import com.manorrock.bengal.message.core.MessageObject;
+
+/**
+ * The emitter for MessageObject.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class MessageObjectEmitter {
+    
+    /**
+     * Emit the Java code.
+     * 
+     * @param messageObject the MessageObject.
+     * @return the Java code.
+     */
+    public String emit(MessageObject messageObject) {
+        StringBuilder javaCode = new StringBuilder();
+        javaCode.append("public class");
+        return javaCode.toString();
+    }
+}
