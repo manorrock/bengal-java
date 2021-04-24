040c03dcc8fac1cac069cdbb94578886bda702d4
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-04-24 06:33:11 -0600

    Fixes issue #18 - Add main method

---
+++ src/main/java/com/manorrock/bengal/message/msg2java/Msg2Java.java
@@ -0,0 +1,13 @@
+package com.manorrock.bengal.message.msg2java;
+
+public class Msg2Java {
+
+    /**
+     * Main entrypoint for msg2java.
+     *
+     * @param arguments the command line arguments.
+     */
+    public static void main(String[] arguments) {
+        System.out.println("Hello World\n");
+    }
+}
