cddd618aca34716d8fb2f96292f4a79486ecfd34
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-04-26 23:17:33 +0000

    Fixes issue #21 - Translate each .bmesg file to a .java file

--- src/main/java/com/manorrock/bengal/message/msg2java/Msg2Java.java
+++ src/main/java/com/manorrock/bengal/message/msg2java/Msg2Java.java
@@ -1,6 +1,59 @@
 package com.manorrock.bengal.message.msg2java;
 
-public class Msg2Java {
+import static java.nio.charset.StandardCharsets.UTF_8;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "bmsg2java", description = "The Bengal Message to Java compiler")
+public class Msg2Java implements Callable<Integer> {
+
+    /**
+     * Stores the source files.
+     */
+    @Parameters(description = "The source file(s) to process")
+    List<String> sourceFiles;
+
+    /**
+     * Call the command.
+     * 
+     * @return the status code.
+     */
+    public Integer call() throws Exception {
+        int result = 0;
+        if (sourceFiles != null) {
+            for (String sourceFile : sourceFiles) {
+                compileFile(sourceFile);
+            }
+        } else {
+            System.out.println("No source files found");
+        }
+        return result;
+    }
+
+    /**
+     * Compile the given file.
+     * 
+     * @throws IOException when an I/O error occurs.
+     */
+    private void compileFile(String sourceFile) throws IOException {
+        System.out.println("Compiling - " + sourceFile);
+        String sourceContent = new String(Files.readAllBytes(Paths.get(sourceFile)), UTF_8);
+        String destinationFilename = sourceFile;
+        destinationFilename = destinationFilename.substring(0, destinationFilename.lastIndexOf("."));
+        destinationFilename = destinationFilename + ".java";
+        PrintWriter destinationWriter = new PrintWriter(new FileWriter(new File(destinationFilename)));
+        destinationWriter.println(sourceContent);
+        destinationWriter.flush();
+    }
 
     /**
      * Main entrypoint for msg2java.
@@ -8,6 +61,6 @@ public class Msg2Java {
      * @param arguments the command line arguments.
      */
     public static void main(String[] arguments) {
-        System.out.println("Hello World\n");
+        System.exit(new CommandLine(new Msg2Java()).execute(arguments));
     }
 }
